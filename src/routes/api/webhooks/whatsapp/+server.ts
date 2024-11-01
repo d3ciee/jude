3663381b233e82e7d9891228cfc068dabe4c09f3
d3ciee <decefemz@gmail.com>
2024-11-01 10:25:08 +0200

    add GET

--- src/routes/api/webhooks/whatsapp/+server.ts
+++ src/routes/api/webhooks/whatsapp/+server.ts
@@ -1,5 +1,14 @@
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ request, locals }) => {
+    try {
+        console.dir(request.json(), { depth: Infinity });
+    } catch (e) {
+
+    }
+    return new Response();
+}
+
+export const GET: RequestHandler = async ({ request, locals }) => {
     return new Response();
 }
