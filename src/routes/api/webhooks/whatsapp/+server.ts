0940f17badbaadde38dc0357cc9bc3c2a725f8cd
d3ciee <decefemz@gmail.com>
2024-11-01 11:37:34 +0200

    whatsapp fakery

--- src/routes/api/webhooks/whatsapp/+server.ts
+++ src/routes/api/webhooks/whatsapp/+server.ts
@@ -1,5 +1,6 @@
 import { WhatsappSession } from '$lib/server/db/schema';
 import genId from '$lib/utils/gen-id';
+import { eq } from 'drizzle-orm';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ request, locals }) => {
@@ -28,41 +29,20 @@ export const POST: RequestHandler = async ({ request, locals }) => {
         }
 
         if (session.step == 1) {
+            locals.db.update(WhatsappSession).set({ step: 2 }).where(eq(WhatsappSession.id, session.id))
             await locals.providers.whatsapp.send({ msisdn, message: "hello, please send your member number" });
         } else if (session.step == 2) {
-
+            locals.db.update(WhatsappSession).set({ step: 3 }).where(eq(WhatsappSession.id, session.id))
             await locals.providers.whatsapp.send({ msisdn, message: "great, now please send your documents, type done when finished" });
+        } else if (session.step == 3) {
+            //check if the message is of type image and upload to s3
+            if (false) {
+
+            }
+
         }
 
-
-
-
-        // if (message == "hie") {
-        //     await locals.providers.whatsapp.send({ msisdn, message: "hello, how can i help you today?" });
-        // } else if message.contains() {
-
-
-
-        // const WhatsappSession = sqliteTable("whatsapp_session", {
-        //     id: text("id").notNull().primaryKey(),
-        //     createdAt: integer("created_at").notNull(),
-        //     expiresAt: integer("expires_at").notNull(),
-        //     userId: text("user_id").notNull().references(() => User.id),
-        //     msisdn: text("msisdn").notNull(),
-        //     step: integer("step").default(0),
-        //     provider: text("provider").notNull(),
-        //     status: text("status", { enum: ["active", "inactive"] }).notNull(),
-        //     metadata: text("metadata", { mode: "json" })
-        // })
-
-
-        // }
-
-
-
-    } catch (e) {
-
-    }
+    } catch (e) { }
     return new Response();
 }
 
@@ -73,3 +53,29 @@ export const GET: RequestHandler = async ({ request, locals, url }) => {
 
     return new Response(challenge);
 }
+
+
+
+
+// if (message == "hie") {
+//     await locals.providers.whatsapp.send({ msisdn, message: "hello, how can i help you today?" });
+// } else if message.contains() {
+
+
+
+// const WhatsappSession = sqliteTable("whatsapp_session", {
+//     id: text("id").notNull().primaryKey(),
+//     createdAt: integer("created_at").notNull(),
+//     expiresAt: integer("expires_at").notNull(),
+//     userId: text("user_id").notNull().references(() => User.id),
+//     msisdn: text("msisdn").notNull(),
+//     step: integer("step").default(0),
+//     provider: text("provider").notNull(),
+//     status: text("status", { enum: ["active", "inactive"] }).notNull(),
+//     metadata: text("metadata", { mode: "json" })
+// })
+
+
+// }
+
+
